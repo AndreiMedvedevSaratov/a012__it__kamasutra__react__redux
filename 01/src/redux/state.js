d99825a8ed089971e643198d8df9d2d0530ebe6b
Andrei Medvedev <andrei_medvedev@epam.com>
2021-02-14 13:52:49 +0400

    feat: 41 lesson last version

--- 01/src/redux/state.js
+++ 01/src/redux/state.js
@@ -2,11 +2,6 @@ import sidebarReducer from './sidebar-reducer';
 import profileReducer from './profile-reducer';
 import dialogsReducer from './dialogs-reducer';
 
-const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
-const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
-const SEND_MESSAGE = 'SEND_MESSAGE';
-
 let store = {
 	_state: {
 		profilePage: {
@@ -53,32 +48,6 @@ let store = {
 		this._state.sidebar = sidebarReducer(this._state.sidebar, action);
 
 		this._callSubscriber(this._state);
-	}
-}
-
-export const addPostActionCreator = () => {
-	return {
-		type: ADD_POST,
-	}
-}
-
-export const updateNewPostTextActionCreator = (text) => {
-	return {
-		type: UPDATE_NEW_POST_TEXT,
-		newText: text,
-	}
-}
-
-export const sendMessageCreator = () => {
-	return {
-		type: SEND_MESSAGE,
-	}
-}
-
-export const updateNewMessageBodyCreator = (body) => {
-	return {
-		type: UPDATE_NEW_MESSAGE_BODY,
-		body: body,
 	}
 }
 
