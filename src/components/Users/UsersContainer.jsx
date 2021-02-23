eb300037f011e42056340688ec0984a906bf0c46
Andrei Medvedev <andrei_medvedev@epam.com>
2021-02-23 17:34:08 +0400

    feat: lesson 62

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -10,7 +10,10 @@ import Preloader from '../Common/Preloader/Preloader';
 class UsersContainer extends React.Component {
 	componentDidMount() {
 		this.props.toggleIsFetching(true);
-		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
+			{
+				withCredentials: true,
+			})
 			.then(response => {
 				this.props.toggleIsFetching(false);
 				this.props.setUsers(response.data.items);
@@ -21,7 +24,10 @@ class UsersContainer extends React.Component {
 	onPageChanged = (pageNumber) => {
 		this.props.setCurrentPage(pageNumber);
 		this.props.toggleIsFetching(true);
-		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
+			{
+				withCredentials: true,
+			})
 			.then(response => {
 				this.props.toggleIsFetching(false);
 				this.props.setUsers(response.data.items);
@@ -31,15 +37,15 @@ class UsersContainer extends React.Component {
 	render() {
 		return <>
 			{this.props.isFetching ? <Preloader /> : null}
-		<Users
-			totalUsersCount={this.props.totalUsersCount}
-			pageSize={this.props.pageSize}
-			currentPage={this.props.currentPage}
-			onPageChanged={this.onPageChanged}
-			users={this.props.users}
-			follow={this.props.follow}
-			unfollow={this.props.unfollow}
-		/>
+			<Users
+				totalUsersCount={this.props.totalUsersCount}
+				pageSize={this.props.pageSize}
+				currentPage={this.props.currentPage}
+				onPageChanged={this.onPageChanged}
+				users={this.props.users}
+				follow={this.props.follow}
+				unfollow={this.props.unfollow}
+			/>
 		</>
 	}
 }
