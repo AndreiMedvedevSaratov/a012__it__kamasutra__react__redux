87f2cda2dcd905beb03c0cda5df63e8ec6b6a979
Andrei Medvedev <andrei_medvedev@epam.com>
2021-02-23 18:37:50 +0400

    feat: lesson 63

---
+++ src/api/api.js
@@ -0,0 +1,25 @@
+import * as axios from 'axios';
+
+const instance = axios.create({
+	withCredentials: true,
+	baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+	headers: {
+		"API-KEY": "ed777fef-dbf4-4b9e-92fc-d8c60a2e1422"
+	}
+});
+
+export const usersAPI = {
+	getUsers(currentPage = 1, pageSize = 10) {
+		return instance.get(`/users?page=${currentPage}&count=${pageSize}`)
+			.then(response => {
+				return response.data;
+			});
+	}
+}
+
+export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+	return instance.get(`follow?page=${currentPage}&count=${pageSize}`)
+		.then(response => {
+			return response.data;
+		});
+}
