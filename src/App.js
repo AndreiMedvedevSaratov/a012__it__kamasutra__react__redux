16d1e9304306acee80fb832e71cb93007b8e4688
Andrei Medvedev <andrei_medvedev@epam.com>
2021-03-20 22:58:21 +0400

    feat: lesson 68

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
+import LoginPage from './components/Login/Login';
 
 function App(props) {
   return (
@@ -27,6 +28,9 @@ function App(props) {
           
           <Route path='/users'
             render={() => <UsersContainer />} />
+          
+          <Route path='/login'
+            render={() => <LoginPage />} />
 
           <Route path='/news' render={() => <News />} />
 
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Redirect } from 'react-router-dom';
 
 const DialogItem = (props) => {
 	let path = "/dialogs/" + props.id;
@@ -37,6 +37,8 @@ const Dialogs = (props) => {
 		let body = e.target.value;
 		props.updateNewMessageBody(body);
 	}
+
+	if (!props.isAuth) return <Redirect to={'/login'} />;
 
 	return (
 		<div className={s.dialogs}>
