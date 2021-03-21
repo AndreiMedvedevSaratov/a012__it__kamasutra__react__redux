391404f70723881e2aa0953f0c6c2f6ecaaa6f91
Andrei Medvedev <andrei_medvedev@epam.com>
2021-03-21 13:48:54 +0400

    feat: lesson 71

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Preloader from '../../../Common/Preloader/Preloader';
 import s from './ProfileInfo.module.css';
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -9,12 +10,13 @@ const ProfileInfo = (props) => {
 
   return (
     <div>
-      <div>
+      {/* <div>
         <img src="https://im0-tub-ru.yandex.net/i?id=3d1eb54f6f662965c44b1205113bb44a&ref=rim&n=33&w=327&h=150"></img>
-      </div>
+      </div> */}
       <div className={s.descriptionBlock}>
         <img src={props.profile.photos.large} />
         avatar + description
+        <ProfileStatus status={'Hello my friends'}/>
       </div>
     </div>
   )
