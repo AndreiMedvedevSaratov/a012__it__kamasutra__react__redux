eafebfc7216289d45d1fd135262162604d899aca
Andrei Medvedev <andrei_medvedev@epam.com>
2021-03-28 17:47:48 +0400

    feat: lesson 75

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Form, Field } from 'react-final-form';
+
+const LoginForm = (props) => {
+	return (
+		<Form
+			initialValues={{
+				form: Login,
+			}}
+		>
+			{() => (
+				<form onSubmit={props.handleSubmit}>
+					<div>
+						<Field placeholder={"Login"} name={"login"} component={"input"} />
+					</div>
+					<div>
+						<Field placeholder={"Password"} name={"password"} component={"input"} />
+					</div>
+					<div>
+						<Field component={"input"} name={"rememberMe"} type={"checkbox"} /> Remember me
+					</div>
+					<div>
+						<button>Login</button>
+					</div>
+				</form>
+			)}
+		</Form>
+	)
+}
 
 const Login = (props) => {
-	return <h1>LOGIN</h1>
+	return <div>
+		<h1>Login</h1>
+		<LoginForm />
+	</div>
 }
 
 export default Login;
