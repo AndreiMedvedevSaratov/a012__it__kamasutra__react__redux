48ed729a2170e832479973406116cc858ea37824
Andrei Medvedev <andrei_medvedev@epam.com>
2021-05-12 18:27:22 +0400

    feat: lesson 81

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { follow, unfollow, setCurrentPage, toggleIsFetching } from '../../redux/users-reducer';
 import { setTotalUsersCount } from '../../redux/users-reducer';
-import { setUsers, toggleFollowingProgress, getUsers } from './../../redux/users-reducer';
+import { setUsers, toggleFollowingProgress, requestUsers } from './../../redux/users-reducer';
 import Users from './Users';
 import Preloader from '../Common/Preloader/Preloader';
 import { withAuthRedirect } from './../../hoc/withAuthRedirect';
 import { compose } from 'redux';
+import { getPageSize, getUsers, getTotalUsersCount, getCurrentPage, getIsFetching, getFollowingInProgress } from './../../redux/users-selectors';
 
 class UsersContainer extends React.Component {
 	componentDidMount() {
@@ -34,14 +35,25 @@ class UsersContainer extends React.Component {
 	}
 }
 
+// let mapStateToProps = (state) => {
+// 	return {
+// 		users: state.usersPage.users,
+// 		pageSize: state.usersPage.pageSize,
+// 		totalUsersCount: state.usersPage.totalUsersCount,
+// 		currentPage: state.usersPage.currentPage,
+// 		isFetching: state.usersPage.isFetching,
+// 		followingInProgress: state.usersPage.followingInProgress,
+// 	}
+// }
+
 let mapStateToProps = (state) => {
 	return {
-		users: state.usersPage.users,
-		pageSize: state.usersPage.pageSize,
-		totalUsersCount: state.usersPage.totalUsersCount,
-		currentPage: state.usersPage.currentPage,
-		isFetching: state.usersPage.isFetching,
-		followingInProgress: state.usersPage.followingInProgress,
+		users: getUsers(state),
+		pageSize: getPageSize(state),
+		totalUsersCount: getTotalUsersCount(state),
+		currentPage: getCurrentPage(state),
+		isFetching: getIsFetching(state),
+		followingInProgress: getFollowingInProgress(state),
 	}
 }
 
@@ -51,6 +63,6 @@ export default compose(
 		unfollow,
 		setCurrentPage,
 		toggleFollowingProgress,
-		getUsers,
+		getUsers: requestUsers,
 	})
 )(UsersContainer);
