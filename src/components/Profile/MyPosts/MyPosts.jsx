49c31deef32c74221485073725369b970f8677d6
Andrei Medvedev <andrei_medvedev@epam.com>
2021-05-31 11:26:02 +0400

    feat: lesson 87

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -27,7 +27,7 @@ let AddNewPostForm = (props) => {
 
 let AddNewPostFormRedux = reduxForm({ form: "ProfileAddNewPostForm" })(AddNewPostForm);
 
-const MyPosts = (props) => {
+const MyPosts = React.memo(props => {
 
   let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount} />);
 
@@ -46,6 +46,6 @@ const MyPosts = (props) => {
       </div>
     </div>
   )
-}
+});
 
 export default MyPosts;
